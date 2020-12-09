42bb74abdbd5e83b738f453c843f32c78bb449c8
amberyuy <yuywang@bu.edu>
2020-12-09 10:23:13 -0500

    add vo class

---
+++ amberwang-coupon-service/coupon-common/src/main/java/com/amberwang/coupon/vo/CommonResponse.java
@@ -0,0 +1,26 @@
+package com.amberwang.coupon.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * <h1>通用响应对象定义</h1>
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommonResponse<T> implements Serializable {
+
+    private Integer code;
+    private String message;
+    private T data;
+
+    public CommonResponse(Integer code, String message) {
+
+        this.code = code;
+        this.message = message;
+    }
+}
