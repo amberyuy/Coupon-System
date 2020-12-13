13a910f89c3fd6f3a1a4cf392c3503af86afc222
amberyuy <yuywang@bu.edu>
2020-12-12 21:32:00 -0500

    Update Constant.java

--- amberwang-coupon-service/coupon-common/src/main/java/com/amberwang/coupon/constant/Constant.java
+++ amberwang-coupon-service/coupon-common/src/main/java/com/amberwang/coupon/constant/Constant.java
@@ -16,18 +16,18 @@ public class Constant {
 
         /** 优惠券码 key 前缀 */
         public static final String COUPON_TEMPLATE =
-                "imooc_coupon_template_code_";
+                "amberwang_coupon_template_code_";
 
         /** 用户当前所有可用的优惠券 key 前缀 */
         public static final String USER_COUPON_USABLE =
-                "imooc_user_coupon_usable_";
+                "amberwang_user_coupon_usable_";
 
         /** 用户当前所有已使用的优惠券 key 前缀 */
         public static final String USER_COUPON_USED =
-                "imooc_user_coupon_used_";
+                "amberwang_user_coupon_used_";
 
         /** 用户当前所有已过期的优惠券 key 前缀 */
         public static final String USER_COUPON_EXPIRED =
-                "imooc_user_coupon_expired_";
+                "amberwang_user_coupon_expired_";
     }
 }
