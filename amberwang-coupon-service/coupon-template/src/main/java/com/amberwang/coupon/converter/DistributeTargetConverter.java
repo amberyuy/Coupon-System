2bf9c231ee56dd2973373b7884884749a2f0aae6
amberyuy <yuywang@bu.edu>
2020-12-14 17:57:18 -0500

    Create DistributeTargetConverter.java

---
+++ amberwang-coupon-service/coupon-template/src/main/java/com/amberwang/coupon/converter/DistributeTargetConverter.java
@@ -0,0 +1,24 @@
+package com.amberwang.coupon.converter;
+
+import com.amberwang.coupon.constant.DistributeTarget;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+/**
+ * <h1>分发目标枚举属性转换器</h1>
+ */
+@Converter
+public class DistributeTargetConverter
+        implements AttributeConverter<DistributeTarget, Integer> {
+
+    @Override
+    public Integer convertToDatabaseColumn(DistributeTarget distributeTarget) {
+        return distributeTarget.getCode();
+    }
+
+    @Override
+    public DistributeTarget convertToEntityAttribute(Integer code) {
+        return DistributeTarget.of(code);
+    }
+}
