46f4d449e4451207796ed90b2fdb64b9a2e8bc2c
amberyuy <yuywang@bu.edu>
2020-12-30 10:12:38 -0500

    Create PermissionSDKConfig.java

---
+++ amberwang-coupon-service/coupon-permission-detector/src/main/java/com/amberwang/coupon/PermissionSDKConfig.java
@@ -0,0 +1,12 @@
+package com.amberwang.coupon;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * <h1>为了引入 permission-sdk 而添加的配置类</h1>
+ */
+@Configuration
+@EnableFeignClients
+public class PermissionSDKConfig {
+}
